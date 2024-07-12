340c97abb0a125902bc87bceff2ca5fc61b44975
VMang0 <valeriakorolkova2703@gmail.com>
2024-07-12 16:24:47 +0300

    feat: hook for tasks logic

---
+++ src/hooks/useTasks.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+
+import { useStorage } from '@hooks/useStorage';
+import { TaskMapType, TaskType } from '@type/tasks';
+import { generateUniqueId } from '@utils/helpers/generateUniqueId';
+
+export const useTasks = ({ date }: { date?: Date } = {}) => {
+  const { addToStorage, getFromStorage } = useStorage();
+  const [tasks, setTasks] = useState<TaskMapType>({});
+
+  const newTask = (): TaskType => ({
+    id: generateUniqueId(),
+    title: 'Task',
+    status: 'in-progress',
+  });
+
+  const updateTasks = (date: Date, updateFn: (tasks: TaskType[]) => TaskType[]): void => {
+    const dateString = date.toString();
+    const updatedTasks = {
+      ...tasks,
+      [dateString]: updateFn(tasks[dateString] || []),
+    };
+    setTasks(updatedTasks);
+    addToStorage<TaskMapType>('tasks', updatedTasks);
+  };
+
+  const addTask = () => {
+    if (!date) return;
+    updateTasks(date, (tasks) => [...tasks, newTask()]);
+  };
+
+  const updateTask = (date: Date, taskId: string, updatedProperties: Partial<TaskType>) => {
+    updateTasks(date, (tasks) => tasks.map((task) => (task.id === taskId ? { ...task, ...updatedProperties } : task)));
+  };
+
+  const deleteTask = (date: Date, taskId: string) => {
+    updateTasks(date, (tasks) => tasks.filter((task) => task.id !== taskId));
+  };
+
+  useEffect(() => {
+    const savedTasks: TaskMapType | null = getFromStorage<TaskMapType>('tasks');
+    if (savedTasks) setTasks(savedTasks);
+  }, []);
+
+  return {
+    tasks,
+    addTask,
+    updateTask,
+    deleteTask,
+  };
+};
