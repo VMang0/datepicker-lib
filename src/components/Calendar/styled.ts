051060a21d812f8d97d187d202b9db2c3eeab7ce
VMang0 <valeriakorolkova2703@gmail.com>
2024-07-12 16:17:11 +0300

    feat: basic calendar for decorator

---
+++ src/components/Calendar/styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const CalendarContainer = styled.div`
+  width: 250px;
+  border: 1px solid #e1e1e1;
+  border-radius: 8px;
+  box-sizing: border-box;
+  text-transform: capitalize;
+`;
