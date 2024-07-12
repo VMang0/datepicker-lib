a121f27629db109df2d2a03b270910e86b78b9e0
VMang0 <valeriakorolkova2703@gmail.com>
2024-07-12 16:23:18 +0300

    feat: constants for calendar

---
+++ src/constants/calendar.ts
@@ -0,0 +1,15 @@
+export enum ArrowDirection {
+  LEFT = 'left',
+  RIGHT = 'right',
+}
+
+export enum FirstWeekDay {
+  MONDAY = 'Monday',
+  SUNDAY = 'Sunday',
+}
+
+export enum CalendarMode {
+  YEAR = 'year',
+  MONTH = 'month',
+  DAY = 'day',
+}
