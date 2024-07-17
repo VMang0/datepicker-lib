35056381e5aea49b9a7281c393ec9734e647db6d
VMang0 <valeriakorolkova2703@gmail.com>
2024-07-18 02:27:04 +0300

    refactor: replace useStorage to useSticky, update logic with tasks

--- src/hooks/useTasks.ts
+++ src/hooks/useTasks.ts
@@ -1,47 +1,40 @@
-import { useEffect, useState } from 'react';
-
 import { TasksStatus } from '@constants/calendar';
-import { useStorage } from '@hooks/useStorage';
+import { useStickyState } from '@hooks/useStickyState';
 import { TaskMapType, TaskType } from '@type/tasks';
 import { generateUniqueId } from '@utils/generateUniqueId';
 
 export const useTasks = ({ date }: { date?: Date } = {}) => {
-  const { addToStorage, getFromStorage } = useStorage();
-  const [tasks, setTasks] = useState<TaskMapType>({});
+  const [tasks, setTasks] = useStickyState<TaskMapType>({}, 'tasks');
 
-  const newTask = (): TaskType => ({
+  const createTask = (): TaskType => ({
     id: generateUniqueId(),
     title: 'Task',
     status: TasksStatus.INPROGRESS,
   });
 
-  const updateTasks = (date: Date, updateFn: (tasks: TaskType[]) => TaskType[]): void => {
+  const applyTaskChanges = (date: Date, updateFn: (tasks: TaskType[]) => TaskType[]): void => {
     const dateString = date.toString();
     const updatedTasks = {
       ...tasks,
       [dateString]: updateFn(tasks[dateString] || []),
     };
     setTasks(updatedTasks);
-    addToStorage<TaskMapType>('tasks', updatedTasks);
   };
 
   const addTask = () => {
     if (!date) return;
-    updateTasks(date, (tasks) => [...tasks, newTask()]);
+    applyTaskChanges(date, (tasks) => [...tasks, createTask()]);
   };
 
   const updateTask = (date: Date, taskId: string, updatedProperties: Partial<TaskType>) => {
-    updateTasks(date, (tasks) => tasks.map((task) => (task.id === taskId ? { ...task, ...updatedProperties } : task)));
+    applyTaskChanges(date, (tasks) =>
+      tasks.map((task) => (task.id === taskId ? { ...task, ...updatedProperties } : task)),
+    );
   };
 
   const deleteTask = (date: Date, taskId: string) => {
-    updateTasks(date, (tasks) => tasks.filter((task) => task.id !== taskId));
+    applyTaskChanges(date, (tasks) => tasks.filter((task) => task.id !== taskId));
   };
-
-  useEffect(() => {
-    const savedTasks: TaskMapType | null = getFromStorage<TaskMapType>('tasks');
-    if (savedTasks) setTasks(savedTasks);
-  }, []);
 
   return {
     tasks,
